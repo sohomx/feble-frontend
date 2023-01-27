b4846498e455f087322066d88c7690ed03bee4d6
Sohom Pal <sohom377@gmail.com>
2023-01-28 00:24:29 +0530

    help me shlok

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,6 +1,11 @@
+import { Flex, Spacer } from "@chakra-ui/react";
 import React from "react";
 
 const Navbar: React.FC = () => {
-  return <div>Navbar</div>;
+  return (
+    <Flex bg="white" height={44} padding="6px 12px">
+      Navbar
+    </Flex>
+  );
 };
 export default Navbar;
