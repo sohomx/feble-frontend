4d9b5fd8526037d7eafa58e5f708c91f00efaec8
Sohom Pal <sohom377@gmail.com>
2023-01-28 19:54:20 +0530

    something is working

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -3,7 +3,10 @@ import React from "react";
 
 const Navbar: React.FC = () => {
   return (
-    <Flex bg="white" height={44} padding="6px 12px">
+    // <Flex bg="white" height="44px" padding="6px 12px">
+    //   Navbar
+    // </Flex>
+    <Flex bg="white" border="1px solid red">
       Navbar
     </Flex>
   );
